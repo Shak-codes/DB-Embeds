9d0921d9648b38d99590a75ae9950a848535f68f
Shakir <shakirrahman81@gmail.com>
2025-09-06 23:32:24 -0400

    Adds config and supabase setup

--- src/config.ts
+++ src/config.ts
@@ -4,11 +4,15 @@ dotenv.config();
 interface Config {
   port: number;
   nodeEnv: string;
+  SUPABASE_URL: string;
+  SUPABASE_KEY: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
+  SUPABASE_URL: process.env.SUPABASE_URL!,
+  SUPABASE_KEY: process.env.SUPABASE_KEY!,
 };
 
 export default config;
